96b876a59514ec610580303c24bc12d292de2b7f
KirkLeroy J. Powell <kirk@thegamestoreapp.com>
2016-11-15 21:03:48 -0600

    Thread Pool Circular Queue with Realloc (job order still not working)

--- unbounded-queue.c
+++ unbounded-queue.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/syscall.h>
+#include <unistd.h>
 
 // Use a struc to reference details pertaining to the queue
 typedef struct queue_object {
@@ -30,6 +32,7 @@ queue_object_t jobs_queue;
 #define INITIAL_QUEUE_SIZE 5
 
 int add_job(int file_descriptor);
+void *handle_task();
 void print_queue(char *message);
 
 int main(){
@@ -47,6 +50,14 @@ int main(){
 
 	print_queue("Queue Initialized");
 
+	// Add two threads to process tasks
+	for(int i=0;i<2;i++){
+		pthread_t worker;
+		pthread_create(&worker,NULL,handle_task,NULL);
+	}
+
+	printf("Threads running ...\n");
+
 	// Add jobs to the queue
 	for(int file_descriptor = 40;file_descriptor<=80;file_descriptor++){
 		if((add_job(file_descriptor)) == -1){
@@ -54,7 +65,9 @@ int main(){
 		}
 	}
 
-	print_queue("Jobs Added");
+	while(1){
+		print_queue("Jobs Added");
+	}
 }
 
 int add_job(int file_descriptor){
@@ -67,20 +80,25 @@ int add_job(int file_descriptor){
 		jobs_queue.latest_job++;
 
 		// Check that latest job doesn't overflow the queue boundry
-		if((size_t)jobs_queue.latest_job >= jobs_queue.buffer_length){
+		if((size_t)jobs_queue.latest_job > jobs_queue.buffer_length){
 			// Check if I can wrap around queue
 			if(jobs_queue.current_job != 0){
 				// Wrap to the front of the queue
 				jobs_queue.latest_job = 0;
+				// Set latest task
+				jobs_queue.queue_buffer[jobs_queue.latest_job] = file_descriptor;
+				// Increment jobs available
+				jobs_queue.jobs_available++;
+				return 0;
 			}
 			else{
 				// Reallocate the size of the queue
 				jobs_queue.buffer_length = jobs_queue.buffer_length * 2;
 				printf("New buffer size? %lu\n", jobs_queue.buffer_length);
-				int *temp_buffer = realloc(jobs_queue.queue_buffer,sizeof(int)*jobs_queue.buffer_length));
+				int *temp_buffer = realloc(jobs_queue.queue_buffer,sizeof(int)*jobs_queue.buffer_length);
 				jobs_queue.queue_buffer = temp_buffer;
 				printf ("Expanded the job queue.\n");
-				if(jobs_queue.current_job > jobs_queue.latest_job){
+				if(jobs_queue.current_job >= jobs_queue.latest_job){
 					// Move head to end of the buffer
 					jobs_queue.current_job = jobs_queue.buffer_length - 1;
 				}
@@ -102,7 +120,42 @@ int add_job(int file_descriptor){
 	// Unlock the queue
 	pthread_mutex_unlock(&jobs_queue.jq_mutex);
 	printf("File descriptor %d inserted into queue.\n", file_descriptor);
+	print_queue("Jobs Added");
+	sleep(1);
 	return 0;
+}
+
+void *handle_task(){
+	int file_descriptor = 0;
+	while(1){
+		pthread_mutex_lock(&jobs_queue.jq_mutex);
+		if(jobs_queue.jobs_available > 0){
+			// Get file descriptor
+			file_descriptor = jobs_queue.queue_buffer[jobs_queue.current_job];
+			printf("Thread #%ld is handling file descriptor %d\n", syscall(SYS_gettid), file_descriptor);
+
+			// Remove from jobs available
+			jobs_queue.jobs_available--;
+			// Check if current job exceeds boundary
+			if((size_t)jobs_queue.current_job >= jobs_queue.buffer_length){
+				// Wrap to start of queue
+				jobs_queue.current_job = 0;
+			}
+			else if(jobs_queue.current_job >= jobs_queue.latest_job && jobs_queue.jobs_available == 0){
+				// Jobs queue is empty
+				// Reset job indexes
+				jobs_queue.current_job = 0;
+				jobs_queue.latest_job = 0;
+			}
+			else{
+				// Move to next job on the queue
+				jobs_queue.current_job++;
+			}
+		}
+		pthread_mutex_unlock(&jobs_queue.jq_mutex);
+		sleep(5);
+	}
+
 }
 
 void print_queue(char *message){
